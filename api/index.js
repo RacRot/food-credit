58d63b5960ae4b1f9176841e69380d44370d9de2
A-725-K <andrea.canepa.12@gmail.com>
2021-04-02 01:30:26 +0200

    Started to implement mongodb schemas of entities

--- api/index.js
+++ api/index.js
@@ -1,12 +1,56 @@
 const http = require('http');
 const express = require('express');
+const mongoose = require('mongoose');
+
+const DBname = process.env.DB_NAME || 'test';
+const DBport = process.env.DB_PORT || 27017;
+const APIport = process.env.API_PORT || 5001;
+
+/****** DATABASE (to refactor) ******/
+mongoose.connect(`mongodb://db:${DBport}/${DBname}`, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on(
+    'error',
+    console.error.bind(console, 'connection error:')
+);
+db.once(
+    'open',
+    () => { console.log('+++++ DB opened!'); }
+);
+
+// ------------------------------------------- //
+const UserSchema = new mongoose.Schema({
+    id: Number,
+    email: String,
+    passwordHash: String,
+    username: String
+    //...configs
+    //  tags: []
+    //  notify
+    //...data
+    //  brokies: []
+    //  status
+});
+
+const CreditsSchema = new mongoose.Schema({
+    id: Number,
+    food: String,
+    quantity: Number,
+    quantityType: String,
+    //terms
+    creditor: UserSchema,
+    debtor: UserSchema,
+    createdAt: Date,
+    closedAt: Date,
+    //tags: []
+    //...data
+});
+
 
 const backend = express();
-const port = process.env.API_PORT || 3333;
-
 const server = http.createServer(backend);
-
 server.listen(
-    port,
-    () => { console.log(`***** API listening on port ===> ${port}`); }
+    APIport,
+    () => { console.log(`***** API listening on port ===> ${APIport}`); }
 )
