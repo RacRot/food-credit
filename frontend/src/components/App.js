60a11954e63b1934eed8c70988ae06bbde56afab
Fabrizio Silvestri <fabrizio.silvestri@yahoo.it>
2021-04-15 21:52:49 +0200

    Convert class component to hooks

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,15 +1,16 @@
-import React, { Component} from 'react';
+import React, { useEffect } from 'react';
 
-import './App.css';
-
-class App extends Component{
-  render(){
-    return(
-      <div className="App">
-        <h1> Hello, World! </h1>
-      </div>
-    );
-  }
-}
+const App = () => {
+  useEffect(() => {
+    fetch('http://localhost:5001/health')
+      .then(response => response.json())
+      .then(data => console.log('The Server said: ', data));
+  })
+  return(
+    <div className="App">
+      <h1> Hello, World! </h1>
+    </div>
+  );
+};
 
 export default App;
