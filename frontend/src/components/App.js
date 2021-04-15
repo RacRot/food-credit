beda411540f7f04cdbcb42bdcc3d6427fb774bb5
Fabrizio Silvestri <fabrizio.silvestri@yahoo.it>
2021-04-15 22:48:52 +0200

    add example usage styled components

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,15 +1,29 @@
 import React, { useEffect } from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
+
 
 const App = () => {
   useEffect(() => {
+    // NOTE: Test connection with api
     fetch('http://localhost:5001/health')
       .then(response => response.json())
       .then(data => console.log('The Server said: ', data));
   })
   return(
-    <div className="App">
-      <h1> Hello, World! </h1>
-    </div>
+    <Wrapper>
+      <Title> Hello, World! </Title>
+    </Wrapper>
   );
 };
 
